fbda0d3f903de6ed6ab6acab751e3d8bb5fc5057
LorenzoPina-dev <67581426+LorenzoPina-dev@users.noreply.github.com>
2025-02-13 14:05:34 +0100

    pie chart generi playlist

--- public/js/playlist.js
+++ public/js/playlist.js
@@ -1,4 +1,4 @@
-let timeline_overlay, map_overlay;
+let timeline_overlay, map_overlay,genres_overlay;
 
 function filter_out_blocked_songs(data) {
     data.tracks = data.tracks.filter(s => s.name !== "" && s.artists[0].name !== "" && s.album.name !== "");
@@ -29,6 +29,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         renderData(data);
     }
 
+    genres_overlay = new GenresOverlay();
     timeline_overlay = new TimelineOverlay();
     map_overlay = new MapOverlay();
 
@@ -36,7 +37,110 @@ document.addEventListener("DOMContentLoaded", async function () {
         const songs = data.tracks;
         let coordUnivoche = new Map();
         const all_artists = songs.map(s => s.artists).flat();
-        window.stocazzo = all_artists;
+        console.log(songs);
+        
+        let generi=new Map();
+        Promise.all(songs.map(async s => {
+            const id_artisti=s.artists[0].id,
+                 nome_artista=s.artists[0].name,
+                 nome_canzone=s.name;
+            let artisti_str_search = await api(`wikidata/elemento?stringa=${encodeURIComponent(id_artisti)}`),
+            codiciArtisti = artisti_str_search.map(p => p.title);
+            if (codiciArtisti.length === 0) {
+                artisti_str_search = await api(`wikidata/elemento?stringa=${encodeURIComponent(nome_artista)}`),
+                codiciArtisti = artisti_str_search.map(p => p.title);
+                if(codiciArtisti.length === 0) {
+                    return s;
+                }
+            }
+            const id_canzoni = (await post_api(
+                'wikidata/gettest', 
+                { codiciArtisti, nomeCanzone:nome_canzone }
+            )).map(c => {
+                const url = c.canzoni.value;
+                return url.slice(url.lastIndexOf('/') + 1,url.length);
+            });
+    
+            if(id_canzoni.length === 0) {
+                return s;
+            } 
+            let dati_canzoni = (await post_api(
+                'wikidata/songById',
+                { codiciCanzoni:id_canzoni, all:false, limit:100 }
+            ))
+            //dati_canzoni=dati_canzoni.filter(v=>!/^Q\d+$/.test(v.canzoniLabel.value));
+            if(dati_canzoni.length === 0) {
+                return s;
+            } 
+            const dati_genres = new Map(),
+                ids = new Map();
+    
+            for(let i=0; i<dati_canzoni.length; i++){
+                const item = dati_canzoni[i],
+                    chiave = item.genereLabel.value;
+                    
+                ids.set(chiave,item.genere.value.slice(item.genere.value.lastIndexOf('/')+1,item.genere.value.length));
+                if (dati_genres.has(chiave)) { 
+                    if (!dati_genres.get(chiave).includes(item.canzoniLabel.value))
+                        dati_genres.set(chiave, [
+                            ...dati_genres.get(chiave), 
+                            item.canzoniLabel.value
+                        ]);
+                }
+                else dati_genres.set(chiave,[item.canzoniLabel.value]);
+            }
+            for (const [key, value] of dati_genres) {
+                if(generi.has(key))
+                    generi.set(key, {
+                        nome:key,
+                        canzoni: [...generi.get(key).canzoni,...value],
+                        id: ids.get(key)
+                    });
+                else generi.set(key, {
+                    nome:key,
+                    canzoni: value,
+                    id: ids.get(key)
+                });
+            }
+        })).then(()=>{
+            const dati_genres=[...generi.values()];
+            if (dati_genres.length === 0)
+                return;
+    
+            const songs_per_genre = dati_genres.map(v => v.canzoni.length),
+                  tot_genres = songs_per_genre.reduce((a,b) => a + b);
+            
+            genres_overlay.enable_external_toggle(e => {
+                if (e.button === 0) {
+                    if (genres_overlay.render_object === null)
+                        genres_overlay.render("Generi", `informazioni wikidata su ${dati_genres.length} composizioni.`,
+                            dati_genres.map(v => {
+                                let ogg = { 
+                                    y: v.canzoni.length/tot_genres*100, 
+                                    genre_name:v.nome,
+                                    song_id: "",
+                                    song_name: ""
+                                };
+                                
+                                return ogg
+                            }),
+                            // data label formatter
+                            function() {
+                                return `<p>${this.point.genre_name}</p>`;
+                            },
+                            // tooltip formatter
+                            function() {
+                                const pct = this.point.y,
+                                      is_int = pct % 1 === 0,
+                                      formatted_pct = is_int ? `${pct}` : pct.toFixed(1);
+                                
+                                return `<p>${this.point.genre_name}: ${formatted_pct}%</p>`;
+                            },null);
+                    else
+                        genres_overlay.toggle_overlay();
+                }
+            })
+        });
         const autoriUnivoci = [...new Map(all_artists.map(a => [a.id, a])).values()];
         console.log(all_artists, [...all_artists])
         Promise.all(autoriUnivoci.map(async s => {
